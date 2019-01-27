da2a1059fff57cb126ef499b5d876f09dba39a2e
hirokichi1992 <plastar14105@gmail.com>
2019-01-27 17:06:56 +0900

    first commit by php

---
+++ helloPhp.php
@@ -0,0 +1,12 @@
+<!DOCUTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <titile>初めてのPHP</titile>
+    </head>
+    <body>
+        <?php
+        echo "Hello PHP!";
+        ?>
+    </body>
+</html>
